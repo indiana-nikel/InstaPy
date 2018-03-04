f29b952986f229482187da7d4bc769712718df46
Tarini Bhatnagar <tarini.bhatnagar@gmail.com>
2018-03-04 02:17:55 -0800

    added testing for flip function

---
+++ instapy/flip.py
@@ -0,0 +1,6 @@
+def flip(img):
+    '''
+    Flips an image in either horizonatl or vertical direction
+    Input: string of path for an image file in .jpg, .jpeg, .png, .tiff format
+    Output: an image file in .jpg, .jpeg, .png, .tiff format
+    '''
