a3bbc73e8d17d21fc0f8bc11985f8d4ab0dca442
Tarini Bhatnagar <tarini.bhatnagar@gmail.com>
2018-03-04 02:18:59 -0800

    fixed argument in function

--- instapy/flip.py
+++ instapy/flip.py
@@ -1,4 +1,4 @@
-def flip(img):
+def flip(img, direction):
     '''
     Flips an image in either horizonatl or vertical direction
     Input: string of path for an image file in .jpg, .jpeg, .png, .tiff format
