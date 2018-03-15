cd6b82d7f5f134b07dec2530404abe905d5fd65b
Indiana Nikel <indiana-nikel@dhcp-128-189-248-189.ubcsecure.wireless.ubc.ca>
2018-03-15 16:03:33 -0700

    Fix with png

--- instapy/test/test_blur.py
+++ instapy/test/test_blur.py
@@ -13,49 +13,48 @@
 
 import numpy as np
 import skimage.io
-import matplotlib.pyplot as plt
 import pytest
 from InstaPy import blur
 
 # input color: image 1
-input1 = np.array([[[10, 20, 30, 40, 50],
-                    [20, 30, 40, 50, 10],
-                    [30, 40, 50, 10, 20],
-                    [40, 50, 10, 20, 30],
-                    [50, 10, 20, 30, 40]],   #R values
-                   [[110, 120, 130, 140, 150],
-                    [120, 130, 140, 150, 110],
-                    [130, 140, 150, 110, 120],
-                    [140, 150, 110, 120, 130],
-                    [150, 110, 120, 130, 140]],   #G values
-                   [[210, 220, 230, 240, 250],
-                    [220, 230, 240, 250, 210],
-                    [230, 240, 250, 210, 220],
-                    [240, 250, 210, 220, 230],
-                    [250, 210, 220, 230, 240]]   #B values
-                   ])
+input = np.array([[[10, 20, 30, 40, 50],
+                   [20, 30, 40, 50, 10],
+                   [30, 40, 50, 10, 20],
+                   [40, 50, 10, 20, 30],
+                   [50, 10, 20, 30, 40]],   #R values
+                  [[110, 120, 130, 140, 150],
+                   [120, 130, 140, 150, 110],
+                   [130, 140, 150, 110, 120],
+                   [140, 150, 110, 120, 130],
+                   [150, 110, 120, 130, 140]],   #G values
+                  [[210, 220, 230, 240, 250],
+                   [220, 230, 240, 250, 210],
+                   [230, 240, 250, 210, 220],
+                   [240, 250, 210, 220, 230],
+                   [250, 210, 220, 230, 240]]   #B values
+                  ])
 
-skimage.io.imsave("./test_img/blur/input1.jpg", input1)
+skimage.io.imsave("./test_img/blur/input.png", input)
 
 # expected output: blur image 1
-exp_output1 = np.array([[[30, 34.4444, 33.3333],
-                         [34.4444, 33.3333, 26.6666],
-                         [33.3333, 26.6666, 25.5555]],  #R values
-                        [[130, 134.4444, 133.3333],
-                         [134.4444, 133.3333, 126.6666],
-                         [133.3333, 126.6666, 125.5555]],  #G values
-                        [[230, 234.4444, 233.3333],
-                         [234.4444, 233.3333, 226.6666],
-                         [233.3333, 226.6666, 225.5555]]   #B values
-                        ])
+exp_output = np.array([[[30, 34.4444, 33.3333],
+                        [34.4444, 33.3333, 26.6666],
+                        [33.3333, 26.6666, 25.5555]],  #R values
+                       [[130, 134.4444, 133.3333],
+                        [134.4444, 133.3333, 126.6666],
+                        [133.3333, 126.6666, 125.5555]],  #G values
+                       [[230, 234.4444, 233.3333],
+                        [234.4444, 233.3333, 226.6666],
+                        [233.3333, 226.6666, 225.5555]]   #B values
+                       ])
 
 #Check if image is blurred correctly
 
 #Blur
 def test_blur1(img):
-    blur("./test_img/blur/input1.jpg")
-    output = skimage.io.imread("blur.jpg")
-    assert output == exp_output1, "The blur function does not work properly"
+    blur("./test_img/blur/input.png")
+    output = skimage.io.imread("blur.png")
+    assert output == exp_output, "The blur function does not work properly"
 
 #In case the intensity values are not in range of 0-255
 
