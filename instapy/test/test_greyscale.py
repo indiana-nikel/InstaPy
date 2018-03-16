819ebfeaf52c7d4906e5408c444bcf0610fe9dd6
Alex <alex.guoxin@gmail.com>
2018-03-15 18:50:04 -0700

    Edit greyscale

--- instapy/test/test_greyscale.py
+++ instapy/test/test_greyscale.py
@@ -23,7 +23,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pytest
-from InstaPy import greyscale
 
 # test_img1: color image
 test_img1 = np.array([[[ 10,  20,  40], [ 20,  40,  10], [ 40,  10,  20]],
@@ -35,12 +34,12 @@ test_img1_gs = np.array([[[ 19,  19,  19], [ 31,  31,  31], [ 20,  20,  20]],
                          [[ 77,  77,  77], [123, 123, 123], [ 80,  80,  80]],
                          [[115, 115, 115], [184, 184, 184], [121, 121, 121]]], dtype = "uint8")
 
-plt.imsave("./test_img/greyscale/test_img1.jpg", test_img1)
+plt.imsave("test_img1.jpg", test_img1)
 
 # test if the greyscale function works properly
 def test_greyscale1():
-    greyscale("./test_img/greyscale/test_img1.jpg", "./test_img/greyscale/test_img1_gs.jpg")
-    output = plt.imread("./test_img/greyscale/test_img1_gs.jpg")[:, :, :3]
+    greyscale(".test_img1.jpg", "test_img1_gs.jpg")
+    output = plt.imread("test_img1_gs.jpg")[:, :, :3]
     assert (output == test_img1_gs).all(), "The greyscale function does not work properly."
 
 # test_img2: greyscale image
@@ -48,31 +47,31 @@ test_img2 = np.array([[[ 19,  19,  19], [ 31,  31,  31], [ 20,  20,  20]],
                       [[ 77,  77,  77], [123, 123, 123], [ 80,  80,  80]],
                       [[115, 115, 115], [184, 184, 184], [121, 121, 121]]], dtype = "uint8")
 
-plt.imsave("./test_img/greyscale/test_img2.jpg", test_img2)
+plt.imsave("test_img2.jpg", test_img2)
 
 # test if the greyscale function changes a greyscale image
 def test_greyscale2():
-    greyscale("./test_img/greyscale/test_img2.jpg", "./test_img/greyscale/test_img2_gs.jpg")
-    output = plt.imread("./test_img/greyscale/test_img2_gs.jpg")[:, :, :3]
+    greyscale("test_img2.jpg", "test_img2_gs.jpg")
+    output = plt.imread("test_img2_gs.jpg")[:, :, :3]
     assert (output == test_img2).all(), "The greyscale function should not change a greyscale image."
 
 # test of exception handling for milestone 3, not required in this milestone
 def test_non_string_input():
     with pytest.raises(TypeError):
-        greyscale(123, "./test_img/greyscale/test_img1_gs.jpg")
+        greyscale(123, "test_img1_gs.jpg")
 
 def test_nonexistent_input_path():
     with pytest.raises(FileNotFoundError):
-        greyscale("./123/456.jpg", "./test_img/greyscale/test_img1_gs.jpg")
+        greyscale("./123/456.jpg", "test_img1_gs.jpg")
 
 def test_non_image_input_file():
     with pytest.raises(OSError):
-        greyscale("./test_img/greyscale/test.pdf", "./test_img/greyscale/test_img1_gs.jpg")
+        greyscale("test.pdf", "test_img1_gs.jpg")
 
 def test_non_string_output():
     with pytest.raises(TypeError):
-        greyscale("./test_img/greyscale/test_img1.jpg", 123)
+        greyscale("test_img1.jpg", 123)
 
 def test_nonexistent_output_path():
     with pytest.raises(FileNotFoundError):
-        greyscale("./test_img/test_img1.jpg", "./123/456.jpg")
+        greyscale("test_img1.jpg", "./123/456.jpg")
